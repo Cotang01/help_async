72fefe90da927ac6672338af27997956945ddfd6
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2023-11-05 22:07:38 +0500

    add docker compose

--- README.md
+++ README.md
@@ -2,3 +2,38 @@ Python version 3.11.3
 Перед тем как запускать скрипт, все положить в одну папку и нужно в терминале
 перейти в директорию этой папки и от туда запускать
 После в терминале прописать команду python main.py -c 'config.ini'
+
+Действия для запуска бота в Dockerfile:
+(В одной директории должны быть файлы: 
+bot.py config.env  Dockerfile  logger.log  
+logger.py requirements.txt)
+1. docker build -t <ваше название контейнера>
+2. Команда запуска для Linux:
+    docker run -d 
+    -v $(pwd)/logger.log:/app/logger.log 
+    -v $(pwd)/config.env:/app/config.env
+    <ваше название контейнера>
+3. Команда запуска для Windows:
+    docker run -d 
+    -v ${PWD}/logger.log:/app/logger.log 
+    -v ${PWD}/config.env:/app/config.env 
+    <ваше название контейнера>
+
+Если logger.log не оказывается на хостовой машине, то контейнер создаёт его
+в своей файловой системе, но хосту он не передаётся, чтобы передавался, надо
+в конце монтирования файла добавить ":z"
+
+Запуск бота с docker-compose:
+(В одной директории должны быть файлы, если хост Linux:
+bot.py configLin.env logger.py requirements.txt logger.log
+Dockerfile docker-compose.yml)
+(Если хост Windows:
+bot.py configWin.env logger.py requirements.txt logger.log
+Dockerfile docker-compose.yml)
+1. docker-compose up -d
+
+В случае docker-compose если на хосте не оказывается файла, 
+то docker создаёт директорию
+
+Если возникает ошибка "http: invalid Host header":
+snap refresh --revision=2893 docker
--- bot.py
+++ bot.py
@@ -1,31 +1,18 @@
 import asyncio
-from os import getenv
+import configargparse
 from logger import get_logger
-
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 
-from parsers import ParseIni
-
-TOKEN = getenv('TOKEN')
-
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
 
-async def on_startup(_):
-    print('Бот запущен')  # Пишет в консоль
-    await _.send_message(chat_id=767528224, text='Started!')
-
-
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
     print('Started!')
     logger.warning('The bot has been started!')
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
@@ -33,28 +20,31 @@ async def command_start_handler(message: Message) -> None:
 
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+    if message.text == '1+1':
+        await message.answer('3')
+    else:
+        await message.answer('Nope, say 1+1')
 
 
 async def main() -> None:
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
     await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
-    cfg = ParseIni().log_config
-    logger = get_logger(cfg.get('level'),
-                        cfg.get('format'),
-                        cfg.get('filename'))
-    # logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+if __name__ == '__main__':
+    parser = configargparse.ArgParser()
+    parser.add_argument('--log-level', env_var='LOG_LEVEL', required=True,
+                        help='Log messages level')
+    parser.add_argument('--log-format', env_var='LOG_FORMAT', required=True,
+                        help='Log messages format')
+    parser.add_argument('--log-file', env_var='LOG_FILE', required=True,
+                        help='Log file name')
+    parser.add_argument('--token', env_var='TOKEN', required=True,
+                        help='Bot token')
+    config = parser.parse_args()
+
+    logger = get_logger(config.log_level,
+                        config.log_format,
+                        config.log_file)
+
+    bot = Bot(config.token, parse_mode=ParseMode.HTML)
     asyncio.run(main())
