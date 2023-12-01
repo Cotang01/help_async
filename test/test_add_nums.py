2f26a2194fcabaaa710108936640e4d7bb3436ba
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2023-12-01 06:50:09 +0500

    git actions integrate

---
+++ test/test_add_nums.py
@@ -0,0 +1,8 @@
+from add_nums.add_numbers import add_numbers
+
+
+def test_add_numbers():
+    assert add_numbers(2, 2) == 4
+    assert add_numbers(1, 0.36) == 1.36
+    assert add_numbers(-2, 2) == 0
+    assert add_numbers(1, 0.39999) == 1.39999
